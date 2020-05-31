696c75b61450201a9079c67d726e3ec5e4b74b4b
mahender1524 <mahibisht560@gmail.com>
2020-06-01 00:38:03 +0530

    Add new git learning doc

---
+++ AllgitCommand.php
@@ -0,0 +1,8 @@
+1 - git init
+2 - git status
+3 - git add -A
+
+4 - git commit -m('All files Add')
+
+5.  git remote add origin Reponame(https://github.com/mahender1524/gitlearningtest.git)
+6 - git push origin master
---
+++ gitdocumnts.php
@@ -0,0 +1,44 @@
+Git Hub Command ===========>
+				||
+				||
+				user============== 1
+				 ||
+				 ||
+				 ||
+				 ||
+				staging Area (git add) =======> 2
+				 ||
+				 ||
+				 ||
+				 ||
+				snapshort (git commit -m) agr a1, a2, a3 A3 file m koi error aa gai to hum git commity s back ja skte hai
+				||
+				||
+				||
+				||git remote add origin Reponame  (link kr dena apne project ko )
+
+					eponame ( https://github.com/mahender1524/gitlearningtest.git)
+
+
+
+git init (add git)Git project  bnanata hai
+git add (staging area ) => file btne k loye kn si file hai kn si file nhi hai  (git add -a)
+
+git  config  --global user.email "mahibiosht560@gmail.com"
+git config    --global user.name mahender
+
+1 . ls command ( show all files in current project)
+
+
+
+
+
+============ Command ============
+
+1 .  git init   (create git project)
+3. git status   ( check untraked file )
+2 . git add -a  (to add untracked  file in git project)
+
+4 . git commit -m "message pass here"
+5.  git remote add origin Reponame(https://github.com/mahender1524/gitlearningtest.git)
+6.  git push origin master
